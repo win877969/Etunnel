5e2ab4fb79c24b38790f7ca059c6a47d2fd34f0b
win877969 <xpandhita@gmail.com>
2024-10-16 20:48:52 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -19,6 +19,15 @@ export default {
 	 * @param {import("@cloudflare/workers-types").ExecutionContext} ctx
 	 * @returns {Promise<Response>}
 	 */
+	/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 	async fetch(request, env, ctx) {
 		try {
 			userID = env.UUID || userID;
@@ -141,12 +150,27 @@ async function vlessOverWSHandler(request) {
 	})).catch((err) => {
 		log('readableWebSocketStream pipeTo error', err);
 	});
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
-	return new Response(null, {
-		status: 101,
+
+//export default {
+  //async fetch(request, env, ctx) {
+    return new Response('Hello World!');
+  },
+};
+	//return new Response(null, {
+		//status: 101,
 		// @ts-ignore
-		webSocket: client,
-	});
+		//webSocket: client,
+	//});
 }
 
 /**
