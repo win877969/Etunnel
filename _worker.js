0df967ab7f18d3e174345afef8fcb3a5b73f4d9e
win877969 <xpandhita@gmail.com>
2024-10-17 04:21:33 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -669,5 +669,10 @@ ${htmlHead}
 </script>
 </body>
 </html>`;
+	} 
+catch ='(error) {
+        console.error("Error generating VLESS config:", error);
+        return ` <p>Error generating VLESS config. Please try again later.</p>`;
+    }
     } 
 
